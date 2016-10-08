2180a04a768a2807b5b363aa3afdd08699906fb8
Cooper Pellaton <c@cepp.at>
2016-10-08 09:22:16 -0400

    Small error.

--- app.js
+++ app.js
@@ -189,7 +189,7 @@ app.post("/api/nextBus", function(req, res) {
 });
 app.get("/api/trafficData", function(req, res) {
     var requestURL = "http://api.cctraffic.net/feeds/map/Traffic.aspx?id=17&type=incident&max=25&bLat=42.203097639603264%2C42.459441175790076&bLng=-83.25866010742186%2C-82.83293989257811&sort=severity_priority%20asc";
-    var xml = http(requestURL);
+    var xml = request(requestURL);
     console.log("XML: " + xml);
     var jsonTrafficData;
     parseString(xml, function(err, result) {
