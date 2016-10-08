3bd4365c64fcbf60846cdad61bbd8b1c2cc19acb
Cooper Pellaton <c@cepp.at>
2016-10-08 06:21:24 -0400

    Cleanup.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 /**
  * Module dependencies.
  */
-var Promise = require('bluebird');
-var Db = require('mongodb').Db,
+ var Promise = require('bluebird');
+ var Db = require('mongodb').Db,
     MongoClient = require('mongodb').MongoClient,
     Server = require('mongodb').Server,
     ReplSetServers = require('mongodb').ReplSetServers,
@@ -30,22 +30,13 @@ const passport = require('passport');
 const expressValidator = require('express-validator');
 const expressStatusMonitor = require('express-status-monitor');
 var geoTools = require('geo-tools');
-// const sass = require('node-sass-middleware');
-// const multer = require('multer');
-// const upload = multer({ dest: path.join(__dirname, 'uploads') });
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
 dotenv.load({
     path: '.env'
 });
-/**
- * Controllers (route handlers).
- */
-// const homeController = require('./controllers/home');
-// const userController = require('./controllers/user');
-// const apiController = require('./controllers/api');
-// const contactController = require('./controllers/contact');
+
 /**
  * Create Express server.
  */
@@ -55,7 +46,6 @@ const app = express();
  */
 var db = new Db('stops', new Server('localhost', 27017));
 mongoose.Promise = global.Promise;
-// mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI);
 mongoose.connect('mongodb://localhost/stops');
 mongoose.connection.on('connected', () => {
     console.log('%s MongoDB connection established!', chalk.green('✓'));
@@ -64,6 +54,7 @@ mongoose.connection.on('error', () => {
     console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
     process.exit();
 });
+
 var stopSchema = new mongoose.Schema({
     stop_name: String,
     stop_lat: String,
@@ -74,15 +65,8 @@ var Stops = mongoose.model('Stops', stopSchema);
  * Express configuration.
  */
 app.set('port', process.env.PORT || 3000);
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'pug');
 app.use(expressStatusMonitor());
 app.use(compression());
-// app.use(sass({
-//   src: path.join(__dirname, 'public'),
-//   dest: path.join(__dirname, 'public')
-// }));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
@@ -133,7 +117,9 @@ app.post('/location/', function(req, res) {
     console.log(req.body); //should be JSON
     res.send(distSort(req.body));
 });
+
 var promiseMongo = Promise.promisifyAll(Db);
+
 // var stopsPromise = new Promise(function(resolve, reject){
 //     db.open(function(err, db) {
 //         var collection = db.collection('stops');
@@ -143,26 +129,28 @@ var promiseMongo = Promise.promisifyAll(Db);
 // }).then(function(collection){
 //     return (collection.find());
 // });
+
 var distSort = function calculateDistance(location) {
     var distanceList = [];
     db.open(function(err, db) {
         var collection = db.collection('stops');
-        collection.find().then(function(contents) {
-            console.log(contents);
-            for (stop in contents) {
-                var object = [stop[1], stop[2]];
-                distanceList.push(stop[0], distance(object, location));
-            }
-            var sortedVals = function getSortedKeys(distanceList) {
-                var keys = [];
-                for (var key in obj) keys.push(key);
-                return keys.sort(function(a, b) {
-                    return obj[a] - obj[b]
-                });
-            }
-            return sortedVals;
-        })
-    });
+        collection.find()
+    }).then(function(contents){
+        console.log(contents);
+        for (stop in contents) {
+            var object = [stop[1], stop[2]];
+            distanceList.push(stop[0], distance(object, location));
+        }
+        var sortedVals = function getSortedKeys(distanceList) {
+            var keys = [];
+            for (var key in obj) keys.push(key);
+            return keys.sort(function(a, b) {
+                return obj[a] - obj[b]
+            });
+        }
+        return sortedVals;
+    });   
+
     // Promise.props({
     //     stops : stopsPromise
     // }).then(function(result){
